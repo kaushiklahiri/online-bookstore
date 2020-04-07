ecc673b1d6b7c1a91fb0d12bf55d98de39ab62ad
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2020-04-07 22:19:49 +0530

    Changes made

--- onlinebookstore/src/main/java/in/kaushiklahiri/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/in/kaushiklahiri/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,9 +1,15 @@
 package in.kaushiklahiri.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableAutoConfiguration(exclude = {HypermediaAutoConfiguration.class})
 @SpringBootApplication
+@EnableSwagger2
 public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
